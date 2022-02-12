9af0e308ec11f7d0883d08c4cedaf6e105327817
Heikel <heikel.gonzalez@oracle.com>
2022-02-12 16:37:25 -0300

    Update to be able to input abi and contrac for specific collection

--- readme.md
+++ readme.md
@@ -1,3 +1,13 @@
+This app retrieves all NFT from a wallet using OpenSea API.
+
 Heroku live app: https://ntf-collection-explorer.herokuapp.com/
 
 Buildpack exctracted from: https://github.com/heroku/heroku-buildpack-static.git
+
+OpenSea Doc: https://docs.opensea.io/reference/getting-assets
+
+IPFS Doc: https://docs.ipfs.io/how-to/address-ipfs-on-web/#dweb-addressing-in-brief
+
+Need to be conected via Metamask or other web3 provider to the ETH network.
+
+This app is using a random NFT collection abi, so it is not mandatory to input one. However if you want to explore a scpecific collection just enter the abi.
--- src/index.js
+++ src/index.js
@@ -1,17 +1,26 @@
-import SpacePunksTokenABI from "./token_abi.js"
+import TokenABI from "./token_abi.js"
 
 document.addEventListener("DOMContentLoaded", () => {
     const web3 = new Web3(window.ethereum)
-
+    
     document.getElementById("load_button").addEventListener("click", async () => {
-        const contract = new web3.eth.Contract(SpacePunksTokenABI, "0x45DB714f24f5A313569c41683047f1d49e78Ba07")
         const walletAddress = document.getElementById("wallet_address").value
+        const token_abi = document.getElementById("token_abi").value
+        const contract_address = document.getElementById("contract_address").value
+        if(!token_abi) {
+            token_abi = TokenABI
+        }
+        if(!contract_address) {
+            contract_address = '0x45DB714f24f5A313569c41683047f1d49e78Ba07'
+        }
+
+        const contract = new web3.eth.Contract(token_abi, contract_address)
         contract.defaultAccount = walletAddress
-        const spacePunksBalance = await contract.methods.balanceOf(walletAddress).call()
+        const walletBalance = await contract.methods.balanceOf(walletAddress).call()
 
         document.getElementById("nfts").innerHTML = ""
 
-        for (let i = 0; i < spacePunksBalance; i++) {
+        for (let i = 0; i < walletBalance; i++) {
             const tokenId = await contract.methods.tokenOfOwnerByIndex(walletAddress, i).call()
 
             let tokenMetadataURI = await contract.methods.tokenURI(tokenId).call()
@@ -22,13 +31,13 @@ document.addEventListener("DOMContentLoaded", () => {
 
             const tokenMetadata = await fetch(tokenMetadataURI).then((response) => response.json())
 
-            const spacePunkTokenElement = document.getElementById("nft_template").content.cloneNode(true)
-            spacePunkTokenElement.querySelector("h1").innerText = tokenMetadata["name"]
-            spacePunkTokenElement.querySelector("a").href = `https://opensea.io/assets/0x45db714f24f5a313569c41683047f1d49e78ba07/${tokenId}`
-            spacePunkTokenElement.querySelector("img").src = tokenMetadata["image"]
-            spacePunkTokenElement.querySelector("img").alt = tokenMetadata["description"]
+            const tokenElement = document.getElementById("nft_template").content.cloneNode(true)
+            tokenElement.querySelector("h1").innerText = tokenMetadata["name"]
+            tokenElement.querySelector("a").href = `https://opensea.io/assets/0x45db714f24f5a313569c41683047f1d49e78ba07/${tokenId}`
+            tokenElement.querySelector("img").src = tokenMetadata["image"]
+            tokenElement.querySelector("img").alt = tokenMetadata["description"]
 
-            document.getElementById("nfts").append(spacePunkTokenElement)
+            document.getElementById("nfts").append(tokenElement)
         }
     })
 })
