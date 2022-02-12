17ba9f9a25af893633c95b6a78c2cf319d793f6b
Heikel <heikel.gonzalez@oracle.com>
2022-02-12 20:23:56 -0300

    Code refactor

--- src/index.js
+++ src/index.js
@@ -4,11 +4,9 @@ document.addEventListener("DOMContentLoaded", () => {
     const web3 = new Web3(window.ethereum)
     
     document.getElementById("load_button").addEventListener("click", async () => {
-        const walletAddress = document.getElementById("wallet_address").value
+        const wallet_address = document.getElementById("wallet_address").value
         let token_abi = document.getElementById("token_abi").value
         let contract_address = document.getElementById("contract_address").value
-        console.log(token_abi)
-        console.log(contract_address)
         if(!token_abi) {
             token_abi = TokenABI
         } else {
@@ -17,33 +15,29 @@ document.addEventListener("DOMContentLoaded", () => {
         if(!contract_address) {
             contract_address = '0x45DB714f24f5A313569c41683047f1d49e78Ba07'
         }
-        console.log(token_abi)
-        console.log(contract_address)
         const contract = new web3.eth.Contract(token_abi, contract_address)
-        contract.defaultAccount = walletAddress
-        const walletBalance = await contract.methods.balanceOf(walletAddress).call()
+        const wallet_balance = await contract.methods.balanceOf(wallet_address).call()
 
         document.getElementById("nfts").innerHTML = ""
 
-        for (let i = 0; i < walletBalance; i++) {
-            const tokenId = await contract.methods.tokenOfOwnerByIndex(walletAddress, i).call()
+        for (let i = 0; i < wallet_balance; i++) {
+            const token_id = await contract.methods.tokenOfOwnerByIndex(wallet_address, i).call()
 
-            let tokenMetadataURI = await contract.methods.tokenURI(tokenId).call()
+            let token_metadata_uri = await contract.methods.tokenURI(token_id).call()
 
-            if (tokenMetadataURI.startsWith("ipfs://")) {
-                tokenMetadataURI = `https://ipfs.io/ipfs/${tokenMetadataURI.split("ipfs://")[1]}`
+            if (token_metadata_uri.startsWith("ipfs://")) {
+                token_metadata_uri = `https://ipfs.io/ipfs/${token_metadata_uri.split("ipfs://")[1]}`
             }
 
-            const tokenMetadata = await fetch(tokenMetadataURI).then((response) => response.json())
-            console.log(tokenMetadata)
+            const token_metadata = await fetch(token_metadata_uri).then((response) => response.json())
 
-            const tokenElement = document.getElementById("nft_template").content.cloneNode(true)
-            tokenElement.querySelector("h1").innerText = tokenMetadata["name"]
-            tokenElement.querySelector("a").href = `https://opensea.io/assets/${contract_address}/${tokenId}`
-            tokenElement.querySelector("img").src = tokenMetadata["image"]
-            tokenElement.querySelector("img").alt = tokenMetadata["description"]
+            const token_element = document.getElementById("nft_template").content.cloneNode(true)
+            token_element.querySelector("h1").innerText = token_metadata["name"]
+            token_element.querySelector("a").href = `https://opensea.io/assets/${contract_address}/${token_id}`
+            token_element.querySelector("img").src = token_metadata["image"]
+            token_element.querySelector("img").alt = token_metadata["description"]
 
-            document.getElementById("nfts").append(tokenElement)
+            document.getElementById("nfts").append(token_element)
         }
     })
 })
